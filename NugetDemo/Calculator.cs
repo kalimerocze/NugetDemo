90a9d2fc81b47e96356002d881e3c3fc5a5ca75b
Adam Jendrulek <adam.jendrulek@gmail.com>
2022-12-03 18:19:17 +0100

    Přidejte soubory projektu.

---
+++ NugetDemo/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace NugetDemo
+{
+    public class Calculator
+    {
+
+
+            public static double Add(double x, double y)
+            {
+                return x + y;
+            }
+
+            public static double Subtract(double x, double y)
+            {
+                return x - y;
+            }
+
+        }
+    }
